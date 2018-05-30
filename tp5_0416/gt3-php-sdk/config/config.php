6aef03f73f7c38ec6458ce7e9f2baa16521a96d7
huzhengen <huzhengen@163.com>
2018-05-30 15:11:30 +0800

    0530

---
+++ tp5_0416/gt3-php-sdk/config/config.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+*  Geetest配置文件
+* @author Tanxu
+*/
+// define("CAPTCHA_ID", "b46d1900d0a894591916ea94ea91bd2c");
+// define("PRIVATE_KEY", "36fc3fe98530eea08dfc6ce76e3d24c4");
+
+define("CAPTCHA_ID", "fab220aa31930021206234cf3cab9f88");
+define("PRIVATE_KEY", "1fc9a5f51d2da075123ceae092cf74b7");
+
+ ?>
